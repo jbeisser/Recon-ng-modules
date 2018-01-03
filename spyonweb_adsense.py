06d960311772038668470fffe8c97ed487973529
jose nazario <jose.monkey.org@gmail.com>
2018-01-03 09:35:37 -0500

    fix pathing in JSON

--- spyonweb_adsense.py
+++ spyonweb_adsense.py
@@ -8,7 +8,7 @@ class Module(BaseModule):
         'description': 'Parses the SpyOnWeb data for shared Google Adsense identifiers, looking for other domains that share the Google Adsense identifier. Updates the \'domains\' table with the results.',
         'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL',
     }
-
+    
     def module_run(self, domains):
         api_secret = self.get_key('spyonweb_secret')
         summary_url = 'https://api.spyonweb.com/v1/summary/{}?access_token={}'
@@ -18,7 +18,7 @@ class Module(BaseModule):
             domainresp = self.request(summary_url.format(domain, api_secret))
             if domainresp.json['status'] != 'found':
                 continue
-            adsense = domainresp.json['result']['summary'].get('adsense', {})
+            adsense = domainresp.json['result']['summary'][domain]['items'].get('adsense', {})
             for aid in adsense.keys():
                 resp = self.request(adsense_url.format(aid, api_secret))
                 for k,data in resp.json['result']['adsense'].iteritems():
