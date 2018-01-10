1907b26b05cfc710dda5e6358ce3df97f597004b
jose nazario <jose.monkey.org@gmail.com>
2018-01-10 06:49:15 -0700

    fix hostname formatting for None

--- dnstrails.py
+++ dnstrails.py
@@ -8,6 +8,12 @@ class Module(BaseModule):
         'description': 'Retrieves hosts sharing the same IP from the DNSTrails data set. Updates the \'hosts\' table.',
         'query': 'SELECT DISTINCT ip_address FROM hosts WHERE ip_address IS NOT NULL',
     }
+    
+    def _fmt_hostname(self, host, domain):
+        if host is not none:
+            return '{}.{}'.format(host, domain)
+        else:
+            return domain
 
 
     def module_run(self, ips):
@@ -16,6 +22,6 @@ class Module(BaseModule):
             url = 'https://app.securitytrails.com/api/search/by_type/ip/{}'.format(ip)
             resp = self.request(url)
             if resp.status_code == 200:
-                hosts = map(lambda x: '{}.{}'.format(x['host'], x['domain']), resp.json['result']['items'])
+                hosts = map(lambda x: self._fmt_hostname(x['host'], x['domain']), resp.json['result']['items'])
                 for host in hosts:
                     self.add_hosts(host=host, ip_address=ip) 
