ddbb65a9b5513736fcadb6cfd14a96bae6d782b5
jose nazario <jose.monkey.org@gmail.com>
2018-02-14 15:19:56 -0500

    Create passivetotal_domains.py

---
+++ passivetotal_domains.py
@@ -0,0 +1,38 @@
+from recon.core.module import BaseModule
+import requests
+from urlparse import urlparse
+import re
+
+class Module(BaseModule):
+
+    meta = {
+        'name': 'PassiveTotal Domain Enumerator',
+        'author': 'j nazario <jose@monkey.org>',
+        'description': 'Leverages the RiskIQ Passive Total API to list DNS domain based no contact emails. Updates the \'domains\' table with the results. Requires your account API username and secret, obtain at https://www.passivetotal.org/settings.',
+        'required_keys': ['passivetotal_username', 'passivetotal_secret'],
+        'query': 'SELECT DISTINCT email FROM contacts WHERE email IS NOT NULL',
+        }
+
+    def get_passivetotal_whois(self, query, field):
+        username = self.get_key('passivetotal_username')
+        key = self.get_key('passivetotal_secret')
+        auth = (username, key)
+        base_url = 'https://api.passivetotal.org'
+        path = '/v2/whois/search?field={0}'.format(field)
+        url = base_url + path
+        data = {'query': query, 'field': field}
+        response = requests.get(url, auth=auth, json=data)
+        data = response.json()
+        res = []
+        if not data.has_key('results'):
+            self.error(data)
+        for result in data.get('results', []):
+            res.append(result['domain'])
+        return res
+
+    def module_run(self, emails):
+        for email in emails:
+            self.heading(email, level=0)
+            results = self.get_passivetotal_whois(email, 'email')
+            for domain in results:
+                self.add_domains(domain)
