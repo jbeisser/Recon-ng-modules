7210cbe2e234028f1aec182acd6b9b0323952e76
jose nazario <jose.monkey.org@gmail.com>
2018-06-04 09:18:59 -0400

    Create whoxy.py

---
+++ whoxy.py
@@ -0,0 +1,23 @@
+from recon.core.module import BaseModule
+
+class Module(BaseModule):
+    meta = {
+        'name': 'WhoXY Domain Enumeration',
+        'author': 'Jose Nazario',
+        'description': 'Uses WhoXY to update the \'domains\' tables.',
+        'comments': (
+            'This module uses the WhoXY API to query by contact email ',
+            'information to identify other domain names they have registered.'
+        ),
+        'required_keys': ['whoxy_key'],
+        'query': 'SELECT DISTINCT email FROM contacts WHERE email IS NOT NULL'
+    }
+
+    def module_run(self, emails):
+        api_key = self.get_key('whoxy_key')
+        url = 'http://api.whoxy.com/?key={0}&reverse=whois&email={1}'
+        for email in emails:
+            resp = self.request(url.format(api_key, email))
+            if resp.status_code == 200:
+                for domain in resp.json['search_result']:
+                    self.add_domains(domain['domain_name'])
