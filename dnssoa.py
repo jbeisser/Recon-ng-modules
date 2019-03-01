4b6ea88f27cc69ab76e0587171aba65a789ff961
jose nazario <jose.monkey.org@gmail.com>
2019-03-01 13:44:09 -0500

    Create dnssoa.py

---
+++ dnssoa.py
@@ -0,0 +1,18 @@
+from recon.core.module import BaseModule
+
+import dns.resolver
+
+class Module(BaseModule):
+    meta = {
+        'name': 'DNS SOA record enumerator',
+        'author': 'Jose Nazario',
+        'description': 'Uses DNS SOA records to update the \'contats\' table.',
+        'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL',
+    }
+
+    def module_run(self, domains):
+        for domain in { x.lower() for x in domains }:
+            ans = dns.resolver.query(domain, 'SOA')
+            for a in ans.rrset.items:
+                e = a.to_text().split()[1].replace('.', '@', 1).rstrip('.')
+                self.add_contacts(email=e)
