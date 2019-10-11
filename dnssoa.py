134c8d6eeb610ffb87ce8e042ce72638e01903dd
jose nazario <jose.monkey.org@gmail.com>
2019-10-11 16:03:34 -0400

    add exception handling

--- dnssoa.py
+++ dnssoa.py
@@ -12,7 +12,10 @@ class Module(BaseModule):
 
     def module_run(self, domains):
         for domain in { x.lower() for x in domains }:
-            ans = dns.resolver.query(domain, 'SOA')
+            try:
+                ans = dns.resolver.query(domain, 'SOA')
+            except (dns.resolver.NoAnswer, dns.resolver.NoNameservers, dns.resolver.NXDOMAIN):
+                continue
             for a in ans.rrset.items:
                 e = a.to_text().split()[1].replace('.', '@', 1).rstrip('.')
                 self.add_contacts(email=e)
